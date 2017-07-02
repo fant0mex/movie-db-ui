98a8cea74c5c8684db0bd6177f1039d11200d762
Joe Hobson <joseph.j.hobson@googlemail.com>
2017-07-02 15:29:12 +0100

    fix: adds missing packages

---
+++ src/scripts/lib/parseFetch.js
@@ -0,0 +1,11 @@
+export function checkStatus (response) {
+  if (response.status === 200) {
+    return Promise.resolve(response)
+  } else {
+    return Promise.reject(new Error(response.statusText))
+  }
+}
+
+export function getJSON (response) {
+  return response.json()
+}
